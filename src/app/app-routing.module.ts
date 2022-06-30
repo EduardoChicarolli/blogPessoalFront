c28250945190ff6832be59bdf472dc29eeb6f840
Eduardo <eduardochicarolli48@gmail.com>
2022-06-30 11:36:19 -0300

    Implementações do menu e rodapé

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// SE NAO IMPORTAR AUTOMANTICO TEM Q SER MANUAL
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // {path: '', redirectTo: 'entrar', pathMatch: 'full'} //pagina inicial
+  
+  // {path:'caddastrar', component: CadastrarComponent}, //apaga um pouco e ve se aparece para importar
+  // {path:'entrar', component: EntrarComponent};
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
